0490babf3eda094851630581a96f2b7bbc3e4c80
Eliud Gateri <egateri@gmail.com>
2023-12-06 16:12:11 +0300

    Add update landing page

--- src/main/java/com/moringaschool/student/App.java
+++ src/main/java/com/moringaschool/student/App.java
@@ -1,7 +1,25 @@
 package com.moringaschool.student;
 
+import com.moringaschool.student.config.DBConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sql2o.Connection;
+import static spark.Spark.*;
+import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        port(8080);
+        staticFileLocation("/public");
+        Connection connection = DBConfig.getConnection();
+        get("/", (request, response) -> {
+
+            logger.info("requestRefId = requestID | statusCode = 200  | status = OK | message = visited home  |");
+            return "Welcome to Wildlife Tracker App";
+        });
+
+
     }
 }
