2bb208adf175619fa5b0899396941bbcbde254f0
Eliud Gateri <egateri@gmail.com>
2023-12-06 22:40:14 +0300

    Add index page

--- src/main/java/com/moringaschool/student/App.java
+++ src/main/java/com/moringaschool/student/App.java
@@ -7,6 +7,11 @@ import org.sql2o.Connection;
 import static spark.Spark.*;
 import static spark.Spark.port;
 import static spark.Spark.staticFileLocation;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
@@ -14,10 +19,13 @@ public class App {
         port(8080);
         staticFileLocation("/public");
         Connection connection = DBConfig.getConnection();
-        get("/", (request, response) -> {
 
+        get("/", (request, response) -> {
+            HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+            Map<String, Object> payload = new HashMap<>();
+            ModelAndView model = new ModelAndView(payload, "index.hbs");
             logger.info("requestRefId = requestID | statusCode = 200  | status = OK | message = visited home  |");
-            return "Welcome to Wildlife Tracker App";
+            return engine.render(model);
         });
 
 
